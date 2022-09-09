4e8c982bd8ca27898b60e76b2993288a62e92238
palinggbt <112263726+palinggbt@users.noreply.github.com>
2022-09-09 12:22:09 +0800

    Update esce.js

--- plugins/esce.js
+++ plugins/esce.js
@@ -3,15 +3,7 @@
 
 let handler = async (m, { conn, command }) => {
     conn.reply(m.chat, `*BETA V2.0.1.6*
-• *Multi Auth ( Multiple File Session )*
-• https://github.com/BOTCAHX/RT-X-MD
-• Script ini mempunyai sesi yang banyak
-• Maka Saran gunakan replit atau jenis run yang bisa save file terlebih dahulu lalu download file sessions nya.
-• Biasanya memiliki 50 sesi atau bisa lebih
-• Upload sesi tersebut ke github lalu run ulang.
-• *Jika ingin melihat apa itu multiple session lihat link di bawah ini.*
-• *Preview* : https://telegra.ph/file/fd3362fea5132cc59fed2.jpg 
-• *Preview* : https://telegra.ph/file/fa2a66e8cca50f62885ab.jpg
+PRIVATE 
 `, m)
 }
 
